5cd30dbe5f7838da6a5e6d7f701f1bbd9a65cf13
wearehackers160 <82085675+wearehackers160@users.noreply.github.com>
2021-04-07 14:26:45 +0500

    Update bestddos.py

--- bestddos.py
+++ bestddos.py
@@ -20,10 +20,8 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print "Author   : HACKERS"
+print "Facebook : https://www.facebook.com/mustafashahba10"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
